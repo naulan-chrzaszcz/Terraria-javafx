9ea197491f94e042f36b4d44cd85e168f5b41ba5
NaulaN <nonochrzaszcz@gmail.com>
2022-05-09 15:15:36 +0200

    Preparation TallGrass.java Details des classes dans un TODO

--- src/main/java/fr/sae/terraria/tiles/Stone.java
+++ src/main/java/fr/sae/terraria/tiles/Stone.java
@@ -8,5 +8,7 @@ public class Stone extends Tile
              Si le joueur craft la Stone, il ne drop pas de charbon
              Mettre une durabilité
              Le nombre de pierre qui est drop est entre 1 et 4
+             (Optionnel) Faire un bruitage lorsque le joueur le casse
+             (Optionnel) Faire des particules de pierre lorsque on est en train de le casser
     */
 }
---
+++ src/main/java/fr/sae/terraria/tiles/TallGrass.java
@@ -0,0 +1,11 @@
+package fr.sae.terraria.tiles;
+
+public class TallGrass
+{
+    /*
+        TODO: Donne de la fibre au joueur de manière aléatoire
+              Peut ne pas drop ou drop juste 1 (Pas plus)
+              (Optionnel) Faire que l'herbe au passage du joueur provoque un bruitage
+              (Optionnel) Faire une animation lorsque le joueur passe dedans
+     */
+}
