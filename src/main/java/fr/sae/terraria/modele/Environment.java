eb4011ae032d07681e716744be7fa24b2e3e3262
Tiago Narciso <tig2468.a@gmail.com>
2022-05-20 11:08:56 +0200

    correction saut+collision avec la map

--- src/main/java/fr/sae/terraria/modele/Environment.java
+++ src/main/java/fr/sae/terraria/modele/Environment.java
@@ -4,22 +4,15 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 
-import javafx.scene.input.KeyCode;
 
 import javafx.util.Duration;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.ToIntFunction;
+
 
 import fr.sae.terraria.modele.entities.*;
 import fr.sae.terraria.modele.entities.entity.Entity;
-
-import static javafx.scene.input.KeyCode.*;
-
 
 public class Environment
 {
@@ -29,8 +22,8 @@ public class Environment
     private final Player player;
 
     private final int[] elementsSize;
-    private int widthTile;
-    private int heightTile;
+    private final int widthTile;
+    private final int heightTile;
     private int ticks = 0;
 
 
@@ -75,32 +68,24 @@ public class Environment
     private void collide() {
         if (player.offset[0] != 0 || player.offset[1] != 0) {
 
-
-//                if ((Math.abs(futurelocX-e.getX()) < 5 && Math.abs(futurelocY-e.getY()) < 20) || (Math.abs(futurelocX+widthTile-e.getX()) < 5 && Math.abs(futurelocY+heightTile-e.getY()) < 20) )
-//                    player.offset[0] = 0;
-//            }
             if (tileMaps.getTile((int) ((player.getX() + player.offset[0] * player.getVelocity()) / widthTile), (int) ((player.getY() + player.offset[1] * player.getVelocity()) / heightTile)) != 0 || tileMaps.getTile((int) ((player.getX() + player.offset[0] * player.getVelocity()) / widthTile), (int) ((player.getY() + player.offset[1] * player.getVelocity() + heightTile) / heightTile)) != 0 || tileMaps.getTile((int) ((player.getX() + player.offset[0] * player.getVelocity() + widthTile) / widthTile), (int) ((player.getY() + player.offset[1] * player.getVelocity()) / heightTile)) != 0 || tileMaps.getTile((int) ((player.getX() + player.offset[0] * player.getVelocity() + widthTile) / widthTile), (int) ((player.getY() + player.offset[1] * player.getVelocity() + heightTile) / heightTile)) != 0)
                 player.offset[0] = 0;
 
-/*                if (player.offset[1] != 0 && (tileMaps.getTile((int)((player.getX() + player.offset[0] * player.getVelocity()) / widthTile), (int) ((player.getY() + 2) / heightTile)) != 0 || tileMaps.getTile((int)((player.getX()+ widthTile + player.offset[0] * player.getVelocity()) / widthTile), (int) ((player.getY() + 2) / heightTile)) != 0)){
-                    player.offset[1] = 0;
-                }*/
-
         }
+        System.out.println(player.air);
         if (player.air) {
-            System.out.println(player.gravity.timer);
-            System.out.println(player.gravity.flightTime/2);
             double futurePosition = player.gravity.formulaOfTrajectory();
+
+
             if (player.gravity.timer < (player.gravity.flightTime/2)){
-
-                if (tileMaps.getTile((int) (player.getX()/widthTile),(int)(futurePosition/heightTile)) ==0 && tileMaps.getTile((int) ((player.getX()+widthTile)/widthTile),(int)(futurePosition/heightTile)) ==0)
+                if (tileMaps.getTile((int) (player.getX()/widthTile),(int)(futurePosition/heightTile)) ==0 && tileMaps.getTile((int) ((player.getX()+widthTile)/widthTile),(int)(futurePosition/heightTile)) ==0) {
                     player.setY(futurePosition);
-                else player.idle();
-            }else {
+                    player.offset[1] = 1;
+                }
 
-                if (tileMaps.getTile(((int) player.getX() / widthTile), ((int) (player.getY() + heightTile) / heightTile) + 1) == 0 && tileMaps.getTile((int) player.getX() / widthTile + 1, (int) (player.getY() + heightTile) / heightTile + 1) == 0)
-                    player.setY(futurePosition);
-            }
+            }else
+                if (Math.abs((player.getY()) - (((int)player.getY()/heightTile+1)*heightTile)) > 3)
+                    player.fall();
         }
         if (player.offset[1] == 0)
             player.air = false;
