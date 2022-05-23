6c869336699010b474ab2d64eb9df6bd182bccdc
NaulaN <nonochrzaszcz@gmail.com>
2022-05-23 20:10:14 +0200

    Fix Warning "FXML 18"

--- src/main/java/fr/sae/terraria/modele/Environment.java
+++ src/main/java/fr/sae/terraria/modele/Environment.java
@@ -1,30 +1,23 @@
 package fr.sae.terraria.modele;
 
-import fr.sae.terraria.Terraria;
-import fr.sae.terraria.modele.blocks.Tree;
-import fr.sae.terraria.modele.entities.entity.Rect;
-import fr.sae.terraria.vue.View;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
-import java.lang.reflect.Array;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import fr.sae.terraria.modele.entities.*;
 import fr.sae.terraria.modele.entities.entity.Entity;
+import fr.sae.terraria.modele.blocks.Tree;
+import fr.sae.terraria.Terraria;
+import fr.sae.terraria.vue.View;
 
 
 public class Environment
