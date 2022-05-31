da55656dfa4aeb2246bcb60f9c64d86a8b6ae021
Tiago Lousa <tig2468.a@gmail.com>
2022-05-31 16:42:51 +0200

    amélioration jou/nuit

--- src/main/java/fr/sae/terraria/vue/LightView.java
+++ src/main/java/fr/sae/terraria/vue/LightView.java
@@ -2,42 +2,50 @@ package fr.sae.terraria.vue;
 
 import fr.sae.terraria.modele.Clock;
 import fr.sae.terraria.modele.TileMaps;
-import fr.sae.terraria.modele.entities.blocks.Torch;
-import javafx.collections.ObservableList;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class LightView {
     private static final double opacityIter = 0.0017;
 
-    private ObservableList<Torch> torches;
     private Clock clock;
     private Pane filterPane;
-    private double opacityNight;
+    private SimpleDoubleProperty opacityNightAir;
+    private SimpleDoubleProperty opacityNightFade;
     private Shape air;
     private Shape fade;
     private Shape tunnel;
-
 
     public LightView(Clock clock, Pane filterPane, double scaleMultiplicatorHeight, double scaleMultiplicatorWidth) {
 
         this.clock = clock;
         this.filterPane = filterPane;
-        opacityNight = .0;
+        opacityNightAir = new SimpleDoubleProperty(0.0);
+        opacityNightFade = new SimpleDoubleProperty(0.8143);
 
-        air = new Rectangle(scaleMultiplicatorWidth* TileMaps.TILE_DEFAULT_SIZE*30 ,scaleMultiplicatorHeight*TileMaps.TILE_DEFAULT_SIZE*14);
-        fade =  new Rectangle(scaleMultiplicatorWidth* TileMaps.TILE_DEFAULT_SIZE*30,scaleMultiplicatorHeight*TileMaps.TILE_DEFAULT_SIZE);
+        air = new Rectangle(scaleMultiplicatorWidth* TileMaps.TILE_DEFAULT_SIZE*30 ,scaleMultiplicatorHeight*TileMaps.TILE_DEFAULT_SIZE*19);
+        fade =  new Rectangle(scaleMultiplicatorWidth* TileMaps.TILE_DEFAULT_SIZE*30,scaleMultiplicatorHeight*TileMaps.TILE_DEFAULT_SIZE+1);
         tunnel = new Rectangle(scaleMultiplicatorWidth* TileMaps.TILE_DEFAULT_SIZE*30,scaleMultiplicatorHeight*TileMaps.TILE_DEFAULT_SIZE);
 
         air.setFill(Color.web("#0d0d38"));
-        fade.setFill(Color.YELLOW);
-        tunnel.setFill(Color.BLACK);
+        Stop[] stops = new Stop[] { new Stop(0,new Color(0,0,0,0) ), new Stop(1, Color.web("#0d0d38"))};
+        LinearGradient lg1 = new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE, stops);
+        fade.setFill(lg1);
+
+        tunnel.setFill(Color.web("#0d0d38"));
 
         fade.setLayoutY(scaleMultiplicatorHeight*TileMaps.TILE_DEFAULT_SIZE*14);
         tunnel.setLayoutY(scaleMultiplicatorHeight*TileMaps.TILE_DEFAULT_SIZE*15);
 
+        tunnel.opacityProperty().bind(opacityNightFade);
+        fade.opacityProperty().bind(opacityNightFade);
+        air.opacityProperty().bind(opacityNightAir);
         filterPane.getChildren().addAll(air,fade,tunnel);
     }
 
@@ -47,12 +55,14 @@ public class LightView {
 
     private void updateOpacity(int minutes) {
         if (minutes >= Clock.FOUR_PM_INGAME || minutes <= Clock.EIGHT_AM_INGAME) {
-
-            if (minutes >= Clock.FOUR_PM_INGAME && minutes < Clock.ONE_DAY_INGAME) { opacityNight += opacityIter ; }
-            else if (minutes > Clock.MIDNIGHT_INGAME && minutes <= Clock.EIGHT_AM_INGAME) { opacityNight -= opacityIter ; }
-
-            opacityNight = (double) Math.round(opacityNight * 10000) / 10000;
-            filterPane.setStyle("-fx-opacity: " + opacityNight);
+            if (minutes >= Clock.FOUR_PM_INGAME && minutes < Clock.ONE_DAY_INGAME) {
+                opacityNightAir.setValue(opacityNightAir.getValue()+opacityIter);
+                opacityNightFade.setValue(opacityNightFade.getValue()-opacityIter);
+            }
+            else if (minutes > Clock.MIDNIGHT_INGAME && minutes <= Clock.EIGHT_AM_INGAME) {
+                opacityNightAir.setValue(opacityNightAir.getValue()-opacityIter);
+                opacityNightFade.setValue(opacityNightFade.getValue()+opacityIter);
+            }
         }
     }
 }
