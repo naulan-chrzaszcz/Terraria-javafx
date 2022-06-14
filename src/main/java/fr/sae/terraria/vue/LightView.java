0d87356295a54c3f4fbb3e49e336d4761cf7a35c
NaulaN <nonochrzaszcz@gmail.com>
2022-06-14 09:58:39 +0200

    Correction nom fonction

--- src/main/java/fr/sae/terraria/vue/LightView.java
+++ src/main/java/fr/sae/terraria/vue/LightView.java
@@ -61,7 +61,7 @@ public class LightView {
         torches.addListener((ListChangeListener<? super Block>) c -> {
             while(c.next()){
                 this.filterPane.getChildren().clear();
-                addTochLights();
+                addTorchLights();
                 if (c.wasRemoved()){
                     resetShapes();
 
@@ -137,7 +137,7 @@ public class LightView {
         this.actualTunnel.setOpacity(.8);
     }
 
-    private void addTochLights()
+    private void addTorchLights()
     {
         for (int i = 0; i < this.environment.getTorches().size() ; i ++){
             Circle torchLight = new Circle(tileSize*CIRCLE_RAY);
