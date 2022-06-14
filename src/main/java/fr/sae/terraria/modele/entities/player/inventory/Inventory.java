6396cdaa8b10259966ae6c2930698739a702c1f6
NaulaN <nonochrzaszcz@gmail.com>
2022-06-14 11:54:06 +0200

    Fix dépassement limite inventaire

--- src/main/java/fr/sae/terraria/modele/entities/player/inventory/Inventory.java
+++ src/main/java/fr/sae/terraria/modele/entities/player/inventory/Inventory.java
@@ -93,7 +93,8 @@ public class Inventory
                         return;
                 }
                 // Si tous les stacks present sont pleins ou aucune ne correspond à l'objet qui à étais pris, il crée un nouveau stack
-                this.createStack(item);
+                if (this.value.size() < 9)
+                    this.createStack(item);
             }
         }
     }
