11f148a144c484c65f9baa341bf3594abc7f320c
NaulaN <nonochrzaszcz@gmail.com>
2022-06-14 10:54:41 +0200

    Modification systeme de durabilité

--- src/main/java/fr/sae/terraria/modele/entities/blocks/BlockSet.java
+++ src/main/java/fr/sae/terraria/modele/entities/blocks/BlockSet.java
@@ -3,12 +3,22 @@ package fr.sae.terraria.modele.entities.blocks;
 
 public enum BlockSet
 {
-    FLOOR_TOP,
-    FLOOR_RIGHT,
-    FLOOR_LEFT,
-    DIRT,
-    ROCK,
-    TALL_GRASS,
-    TORCH,
-    TREE
+    FLOOR_TOP(3),
+    FLOOR_RIGHT(3),
+    FLOOR_LEFT(3),
+    DIRT(3),
+    ROCK(5),
+    TALL_GRASS(2),
+    TORCH(1),
+    TREE(4);
+
+    private final int durability;
+
+
+    BlockSet(final int durability)
+    {
+        this.durability = durability;
+    }
+
+    public int getDurability() { return this.durability; }
 }
